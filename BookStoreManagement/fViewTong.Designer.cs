b9f6af7e513b345cc4bbad4d4fc225549e09dc7d
Sang1804 <nguyenhoangsang10a42019@gmail.com>
2024-09-09 20:25:39 +0700

    Thêm view tông

---
+++ BookStoreManagement/fViewTong.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace BookStoreManagement
+{
+    partial class fViewTong
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            tÀIKHOẢNToolStripMenuItem = new ToolStripMenuItem();
+            sÁCHToolStripMenuItem = new ToolStripMenuItem();
+            lĨNHVỰCToolStripMenuItem = new ToolStripMenuItem();
+            lOẠISÁCHToolStripMenuItem = new ToolStripMenuItem();
+            tÁCGIẢToolStripMenuItem = new ToolStripMenuItem();
+            hÓAĐƠNToolStripMenuItem = new ToolStripMenuItem();
+            kHOToolStripMenuItem = new ToolStripMenuItem();
+            nHÀXUẤTBẢNToolStripMenuItem = new ToolStripMenuItem();
+            tHỐNGKÊToolStripMenuItem = new ToolStripMenuItem();
+            listView1 = new ListView();
+            columnHeader1 = new ColumnHeader();
+            columnHeader2 = new ColumnHeader();
+            columnHeader3 = new ColumnHeader();
+            columnHeader4 = new ColumnHeader();
+            columnHeader5 = new ColumnHeader();
+            lblNguoiDung = new Label();
+            lblUit = new Label();
+            lblDoanhThuNgay = new Label();
+            lblSLKhach = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { tÀIKHOẢNToolStripMenuItem, sÁCHToolStripMenuItem, lĨNHVỰCToolStripMenuItem, lOẠISÁCHToolStripMenuItem, tÁCGIẢToolStripMenuItem, hÓAĐƠNToolStripMenuItem, kHOToolStripMenuItem, nHÀXUẤTBẢNToolStripMenuItem, tHỐNGKÊToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(986, 28);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // tÀIKHOẢNToolStripMenuItem
+            // 
+            tÀIKHOẢNToolStripMenuItem.Name = "tÀIKHOẢNToolStripMenuItem";
+            tÀIKHOẢNToolStripMenuItem.Size = new Size(100, 24);
+            tÀIKHOẢNToolStripMenuItem.Text = "TÀI KHOẢN";
+            // 
+            // sÁCHToolStripMenuItem
+            // 
+            sÁCHToolStripMenuItem.Name = "sÁCHToolStripMenuItem";
+            sÁCHToolStripMenuItem.Size = new Size(61, 24);
+            sÁCHToolStripMenuItem.Text = "SÁCH";
+            // 
+            // lĨNHVỰCToolStripMenuItem
+            // 
+            lĨNHVỰCToolStripMenuItem.Name = "lĨNHVỰCToolStripMenuItem";
+            lĨNHVỰCToolStripMenuItem.Size = new Size(89, 24);
+            lĨNHVỰCToolStripMenuItem.Text = "LĨNH VỰC";
+            // 
+            // lOẠISÁCHToolStripMenuItem
+            // 
+            lOẠISÁCHToolStripMenuItem.Name = "lOẠISÁCHToolStripMenuItem";
+            lOẠISÁCHToolStripMenuItem.Size = new Size(96, 24);
+            lOẠISÁCHToolStripMenuItem.Text = "LOẠI SÁCH";
+            // 
+            // tÁCGIẢToolStripMenuItem
+            // 
+            tÁCGIẢToolStripMenuItem.Name = "tÁCGIẢToolStripMenuItem";
+            tÁCGIẢToolStripMenuItem.Size = new Size(77, 24);
+            tÁCGIẢToolStripMenuItem.Text = "TÁC GIẢ";
+            // 
+            // hÓAĐƠNToolStripMenuItem
+            // 
+            hÓAĐƠNToolStripMenuItem.Name = "hÓAĐƠNToolStripMenuItem";
+            hÓAĐƠNToolStripMenuItem.Size = new Size(92, 24);
+            hÓAĐƠNToolStripMenuItem.Text = "HÓA ĐƠN";
+            // 
+            // kHOToolStripMenuItem
+            // 
+            kHOToolStripMenuItem.Name = "kHOToolStripMenuItem";
+            kHOToolStripMenuItem.Size = new Size(54, 24);
+            kHOToolStripMenuItem.Text = "KHO";
+            // 
+            // nHÀXUẤTBẢNToolStripMenuItem
+            // 
+            nHÀXUẤTBẢNToolStripMenuItem.Name = "nHÀXUẤTBẢNToolStripMenuItem";
+            nHÀXUẤTBẢNToolStripMenuItem.Size = new Size(130, 24);
+            nHÀXUẤTBẢNToolStripMenuItem.Text = "NHÀ XUẤT BẢN";
+            // 
+            // tHỐNGKÊToolStripMenuItem
+            // 
+            tHỐNGKÊToolStripMenuItem.Name = "tHỐNGKÊToolStripMenuItem";
+            tHỐNGKÊToolStripMenuItem.Size = new Size(95, 24);
+            tHỐNGKÊToolStripMenuItem.Text = "THỐNG KÊ";
+            // 
+            // listView1
+            // 
+            listView1.Alignment = ListViewAlignment.Left;
+            listView1.Columns.AddRange(new ColumnHeader[] { columnHeader1, columnHeader2, columnHeader3, columnHeader4, columnHeader5 });
+            listView1.GridLines = true;
+            listView1.Location = new Point(37, 76);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(895, 352);
+            listView1.TabIndex = 1;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.View = View.Details;
+            // 
+            // columnHeader1
+            // 
+            columnHeader1.Text = "TÊN SÁCH";
+            columnHeader1.Width = 350;
+            // 
+            // columnHeader2
+            // 
+            columnHeader2.Text = "TÁC GIẢ";
+            columnHeader2.Width = 180;
+            // 
+            // columnHeader3
+            // 
+            columnHeader3.Text = "LĨNH VỰC";
+            columnHeader3.Width = 180;
+            // 
+            // columnHeader4
+            // 
+            columnHeader4.Text = "LOẠI SÁCH";
+            columnHeader4.Width = 100;
+            // 
+            // columnHeader5
+            // 
+            columnHeader5.Text = "GIÁ BÌA";
+            columnHeader5.Width = 95;
+            // 
+            // lblNguoiDung
+            // 
+            lblNguoiDung.AutoSize = true;
+            lblNguoiDung.Location = new Point(602, 43);
+            lblNguoiDung.Name = "lblNguoiDung";
+            lblNguoiDung.Size = new Size(105, 20);
+            lblNguoiDung.TabIndex = 2;
+            lblNguoiDung.Text = "NGƯỜI DÙNG:";
+            // 
+            // lblUit
+            // 
+            lblUit.AutoSize = true;
+            lblUit.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblUit.Location = new Point(713, 43);
+            lblUit.Name = "lblUit";
+            lblUit.Size = new Size(28, 20);
+            lblUit.TabIndex = 3;
+            lblUit.Text = "uit";
+            // 
+            // lblDoanhThuNgay
+            // 
+            lblDoanhThuNgay.AutoSize = true;
+            lblDoanhThuNgay.Location = new Point(37, 446);
+            lblDoanhThuNgay.Name = "lblDoanhThuNgay";
+            lblDoanhThuNgay.Size = new Size(194, 20);
+            lblDoanhThuNgay.TabIndex = 4;
+            lblDoanhThuNgay.Text = "DOANH THU TRONG NGAY:";
+            // 
+            // lblSLKhach
+            // 
+            lblSLKhach.AutoSize = true;
+            lblSLKhach.Location = new Point(37, 482);
+            lblSLKhach.Name = "lblSLKhach";
+            lblSLKhach.Size = new Size(138, 20);
+            lblSLKhach.TabIndex = 5;
+            lblSLKhach.Text = "SO LUONG KHACH:";
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(648, 462);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(250, 27);
+            dateTimePicker1.TabIndex = 6;
+            // 
+            // fViewTong
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(986, 545);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(lblSLKhach);
+            Controls.Add(lblDoanhThuNgay);
+            Controls.Add(lblUit);
+            Controls.Add(lblNguoiDung);
+            Controls.Add(listView1);
+            Controls.Add(menuStrip1);
+            Name = "fViewTong";
+            Text = "QUẢN LÝ NHÀ SÁCH";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem tÀIKHOẢNToolStripMenuItem;
+        private ToolStripMenuItem sÁCHToolStripMenuItem;
+        private ToolStripMenuItem lĨNHVỰCToolStripMenuItem;
+        private ToolStripMenuItem lOẠISÁCHToolStripMenuItem;
+        private ToolStripMenuItem tÁCGIẢToolStripMenuItem;
+        private ToolStripMenuItem hÓAĐƠNToolStripMenuItem;
+        private ToolStripMenuItem kHOToolStripMenuItem;
+        private ToolStripMenuItem nHÀXUẤTBẢNToolStripMenuItem;
+        private ToolStripMenuItem tHỐNGKÊToolStripMenuItem;
+        private ListView listView1;
+        private ColumnHeader columnHeader1;
+        private ColumnHeader columnHeader2;
+        private ColumnHeader columnHeader3;
+        private ColumnHeader columnHeader4;
+        private ColumnHeader columnHeader5;
+        private Label lblNguoiDung;
+        private Label lblUit;
+        private Label lblDoanhThuNgay;
+        private Label lblSLKhach;
+        private DateTimePicker dateTimePicker1;
+    }
+}
